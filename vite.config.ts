a39b68ff1190930af6cfcdd78438a53c651ff392
“zhanghf1309” <zhanghf1309@chinaunicom.cn>
2023-08-14 09:08:48 +0800

    s

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
